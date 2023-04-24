68f9db919177f8bd4719aa614cdd5544e343ce7a
TimWolfskin <timwolfskin013@gmail.com>
2023-04-24 15:59:21 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import Gallery from './pages/Gallery'
+import Home from './pages/Home'
+
+
 function App() {
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+    {
+      path: "/gallery",
+      element: <Gallery />,
+    },
+    {
+      path: "/blog",
+      element: <Blog />,
+    },
+  ]);
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/Header.module.scss";
+import LOGO from "../../media/logo.svg";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.header__inner}>
+            <nav className={styles.menu}>
+              <ul className={styles.menu__list}>
+                <li className={styles.menu__listItem}>
+                  <Link className={styles.menu__listIink} to="/about">
+                    About
+                  </Link>
+                </li>
+                <li className={styles.menu__listItem}>
+                  <Link className={styles.menu__listIink} to="gallery">
+                    Gallery
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <Link className={styles.logo} to="/">
+              <img
+                style={{ widows: "30px", height: "30px" }}
+                src={LOGO}
+                alt=""
+              />
+            </Link>
+            <nav className={styles.menu}>
+              <ul className={styles.menu__list}>
+                <li className={styles.menu__listItem}>
+                  <Link className={styles.menu__listIink} to="/blog">
+                    Blog
+                  </Link>
+                </li>
+                <li className={styles.menu__listItem}>
+                  <Link className={styles.menu__listIink} to="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
